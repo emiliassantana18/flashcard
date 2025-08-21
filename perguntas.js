31f843e8721c6eeb932750a1ffd126f664916ca0
emiliassantana18 <santana.emilia@escola.pr.gov.br>
2025-08-21 11:49:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+        'geografia',
+            'Qual é o maior país em território?',
+                'A Rússia.'
+                )
+
+                criaCartao(
+                    'Geografia',
+                        'Qual a capital da França?',
+                            'A capital da França é Paris'
+                            )
+
+                            criaCartao(
+                                'geografia',
+                                    'Qual é o rio mais longo do mundo?',
+                                        'O Rio Nilo.'
+                                        )
+
+                                        criaCartao(
+                                            'geografia',
+                                                'Qual é o maior oceano da Terra?',
+                                                    'O Oceano Pacífico.'
+                                                    )
